e9f9f9a1c67bce32c04d9d9d4b397b20668089fb
bytemuck <bytemuck@gmail.com>
2024-07-26 10:07:29 -0400

    Fixed weird bug in problem 30

--- src/PE/Common.hs
+++ src/PE/Common.hs
@@ -1,4 +1,4 @@
-module PE.Common (digits, divisors, primes, primeFactors, windowed, windowed', fib, fibInfinite) where
+module PE.Common (digits, digitSum, divisors, primes, primeFactors, windowed, windowed', fib, fibInfinite) where
 
 import Data.Char (digitToInt)
 import Data.List (tails, transpose)
@@ -16,6 +16,10 @@ divisors n = [x | x <- [1 .. (n - 1)], n `mod` x == 0]
 
 digits :: (Show a, Integral a) => a -> [a]
 digits n = [fromIntegral (digitToInt x) | x <- show n]
+
+digitSum :: Int -> Int -> Int
+digitSum e 0 = 0
+digitSum e x = (x `mod` 10) ^ e + digitSum e (x `div` 10)
 
 windowed :: (Real a) => Int -> [a] -> [[a]]
 windowed n xs = transpose (take (fromIntegral n) (tails xs))
--- src/PE/P030.hs
+++ src/PE/P030.hs
@@ -1,13 +1,19 @@
 module PE.P030 (p030) where
 
-import PE.Common (digits)
+import Data.Function (on)
+import PE.Common (digitSum, digits)
 
-digitSum :: Int -> Int -> Int
-digitSum e 0 = 0
-digitSum e x = (x `mod` 10) ^ e + digitSum e (x `div` 10)
+-- 10 ^ n <= n * 9 ^ e
+-- (10 ^ n) / n <= 9 ^ e
+-- log9 ((10 ^ n) / n) <= e
+
+-- where n is the number of digits and e the exponent we solving for.
+
+maxValue :: Int -> Int
+maxValue e = (9 ^ e) * last (takeWhile (<= e) [round $ logBase 9 (fromIntegral (10 ^ n) / fromIntegral n) | n <- [1 ..]])
 
 digitsSum :: Int -> [Int]
-digitsSum e = [n | n <- [2 ^ e .. e * 9 ^ e], n == digitSum e n]
+digitsSum e = [n | n <- [2 ^ e .. maxValue e], n == digitSum e n]
 
 p030 :: IO ()
 p030 = print . sum $ digitsSum 5
