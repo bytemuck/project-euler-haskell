a51b0f37d1bed992d144d7437692b4ba1fdfa996
bytemuck <bytemuck@gmail.com>
2024-07-26 10:15:06 -0400

    Fixed equation for problem 30.

--- src/PE/P030.hs
+++ src/PE/P030.hs
@@ -3,14 +3,14 @@ module PE.P030 (p030) where
 import Data.Function (on)
 import PE.Common (digitSum, digits)
 
--- 10 ^ n <= n * 9 ^ e
--- (10 ^ n) / n <= 9 ^ e
--- log9 ((10 ^ n) / n) <= e
+-- (10 ^ n) - 1 <= n * 9 ^ e
+-- ((10 ^ n) - 1) / n <= 9 ^ e
+-- log9 (((10 ^ n) - 1) / n) <= e
 
 -- where n is the number of digits and e the exponent we solving for.
 
 maxValue :: Int -> Int
-maxValue e = (9 ^ e) * last (takeWhile (<= e) [round $ logBase 9 (fromIntegral (10 ^ n) / fromIntegral n) | n <- [1 ..]])
+maxValue e = (9 ^ e) * last (takeWhile (<= e) [round $ logBase 9 (fromIntegral ((10 ^ n) - 1) / fromIntegral n) | n <- [1 ..]])
 
 digitsSum :: Int -> [Int]
 digitsSum e = [n | n <- [2 ^ e .. maxValue e], n == digitSum e n]
